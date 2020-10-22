5c48916cc0f2411d356468d1d96e09edc5e440a3
macbookair <m2003@tut.by>
2020-10-22 16:41:56 +0300

    My function number 2

---
+++ date_autumn.py
@@ -0,0 +1,15 @@
+dates = ("11-27-2006", "12-01-2009", "08-31-2010", "11-28-2008")
+
+
+def date_autumn(dates):
+    la = []
+    max = 0
+    for i in dates:
+        p = i.split("-")
+        if p[0] == "09" or p[0] == "10" or p[0] == "11":
+            la.append(p)
+    for i in la:
+
+
+
+date_autumn(dates)
--- remove_palindroms.py
+++ remove_palindroms.py
@@ -1,7 +1,5 @@
 def remove_palindroms(new_list):
-    o = 0
     for i in new_list:
-        o += 1
         if i.lower() == i[::-1].lower():
-            new_list.pop(o - 1)
-    return (new_list)
+            new_list.remove(i)
+    return new_list
